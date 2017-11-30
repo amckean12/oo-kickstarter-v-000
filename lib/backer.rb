19e6c1540b9563932c40d9039772bdcbe648860f
Alex McKean <amckean@me.com>
2017-11-30 17:23:37 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,6 +1,11 @@
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :title
   def initialize(name) 
     @name = name
+    @backed_projects = []
+  end
+
+  def back_project(title)
+    @backed_projects << title
   end
 end
